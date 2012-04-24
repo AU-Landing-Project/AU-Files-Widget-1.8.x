c5c2b8cc084842df355456924d33b4bb2d373d6e
Matt Beckett <beck24@gmail.com>
2012-04-24 11:30:07 -0600

    Initial Commit

---
+++ au_files_widget/views/default/widgets/filerepo/content.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Elgg bookmarks widget
+ *
+ * @package Bookmarks
+ */
+$widget = $vars['entity'];
+
+// now we can get our content
+$options = eligo_get_display_entities_options($widget);
+$content = elgg_list_entities($options);
+
+
+$content = elgg_list_entities($options);
+
+echo $content;
+
+if ($content) {
+	$url = "file/owner/" . elgg_get_page_owner_entity()->username;
+	$more_link = elgg_view('output/url', array(
+		'href' => $url,
+		'text' => elgg_echo('file:more'),
+		'is_trusted' => true,
+	));
+	echo "<span class=\"elgg-widget-more\">$more_link</span>";
+} else {
+	echo elgg_echo('file:none');
+}
