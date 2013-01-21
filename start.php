d1903c3b941ce61d4bc2ad3d8dff56204801d761
Matt Beckett <beck24@gmail.com>
2013-01-21 12:42:13 -0700

    removes 'all' context keyword

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 
 function au_files_widget_init(){
 	elgg_unregister_widget_type("filerepo");
-	elgg_register_widget_type('filerepo', elgg_echo("file"), elgg_echo("file:widget:description"), 'all,groups', TRUE);
+	elgg_register_widget_type('filerepo', elgg_echo("file"), elgg_echo("file:widget:description"), 'profile,dashboard,index,groups', TRUE);
 }
 
 elgg_register_event_handler("init", "system", "au_files_widget_init");
